fb575be96d1b7237bf614a84a37f3f98b8616a41
Bernhard Wendel <BWendel@gmx.de>
2015-05-30 21:55:19 +0200

    Eine Index-Seite aussenden

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
+
+var express = require('express'),
+    router = express.Router(),
+    path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 module.exports = router;
