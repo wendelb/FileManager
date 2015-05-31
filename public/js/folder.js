d674a8ee8bbd51e55d6bf418a8bd7f7850e4d0f9
Bernhard Wendel <BWendel@gmx.de>
2015-05-31 21:20:44 +0200

    Baum-Struktur, Dateien hochladen

---
+++ public/js/folder.js
@@ -0,0 +1,159 @@
+﻿
+function Folder(name, path) {
+    this.name = name;
+    this.subfolders = [];
+    this.files = [];
+    this.path = path;
+}
+
+Folder.prototype.addFile = function (file) {
+    this.files.push(file);
+}
+
+Folder.prototype.addFolder = function (folder) {
+    this.subfolders.push(folder);
+}
+
+Folder.prototype.addFolderFullName = function (folder) {
+    var fullname = folder.split("/");
+    // Es gibt keine tieferen Verzweigungen
+    if (fullname.length == 1) {
+
+        // Zunächst prüfen, ob der Ordner bereits existiert
+        for (var i = 0; i < this.subfolders.length; i++) {
+            if (this.subfolders[i].name === folder) {
+                return;
+            }
+        }
+
+        // Ordner existiert noch nicht -> erstellen
+        var newFolder = new Folder(folder, this.path + "/" + folder);
+        // und in die Liste einfügen
+        this.addFolder(newFolder);
+    }
+    else {
+        // Es gibt tiefere Strukturen
+        var lastPart = fullname.splice(1).join("/");
+        var firstPart = fullname[0];
+
+        // Richtigen Ordner raussuchen und der Unterstruktur die Bearbeitung übergeben
+        for (var i = 0; i < this.subfolders.length; i++) {
+            if (this.subfolders[i].name === firstPart) {
+                this.subfolders[i].addFolderFullName(lastPart);
+                return;
+            }
+        }
+    }
+}
+
+Folder.prototype.addFileFullName = function (file) {
+    file.structureKey = file.structureKey || file.relpath;
+
+    var filename = file.structureKey;
+
+    var fullname = filename.split("/");
+    // Es gibt keine tieferen Verzweigungen
+    if (fullname.length == 1) {
+
+        // Zunächst prüfen, ob die Datei bereits existiert
+        for (var i = 0; i < this.files.length; i++) {
+            if (this.files[i].filename === filename) {
+                // Wenn ja, dann einfach nur updaten
+                this.files[i] = file;
+                return;
+            }
+        }
+
+        // Ansonsten erstellen
+        this.addFile(file);
+    }
+    else {
+        // Es gibt tiefere Strukturen
+        var lastPart = fullname.splice(1).join("/");
+        var firstPart = fullname[0];
+
+        // Richtigen Ordner raussuchen und der Unterstruktur die Bearbeitung übergeben
+        for (var i = 0; i < this.subfolders.length; i++) {
+            if (this.subfolders[i].name === firstPart) {
+                // Hinweis an die Unterstruktur: Wir haben uns bereits um unseren Teil gekümmert.
+                file.structureKey = lastPart;
+                this.subfolders[i].addFileFullName(file);
+                return;
+            }
+        }
+    }
+}
+
+Folder.prototype.deleteFileFull = function (filename) {
+    var fullname = filename.split("/");
+
+    // Es gibt keine tieferen Verzweigungen -> unsere Datei
+    if (fullname.length == 1) {
+
+        // Datei suchen...
+        for (var i = 0; i < this.files.length; i++) {
+            if (this.files[i].filename === filename) {
+                // ... und entfernen
+                this.files.splice(i, 1);
+                return;
+            }
+        }
+    }
+    else {
+        // Es gibt tiefere Strukturen
+        var lastPart = fullname.splice(1).join("/");
+        var firstPart = fullname[0];
+
+        // Richtigen Ordner raussuchen und der Unterstruktur die Bearbeitung übergeben
+        for (var i = 0; i < this.subfolders.length; i++) {
+            if (this.subfolders[i].name === firstPart) {
+                // Hinweis an die Unterstruktur: Wir haben uns bereits um unseren Teil gekümmert.
+                this.subfolders[i].deleteFileFull(lastPart);
+                return;
+            }
+        }
+    }
+}
+
+Folder.prototype.destroy = function () {
+    this.files = [];
+    this.name = "";
+    this.path = "";
+
+    for (var i = 0; i < this.subfolders.length; i++) {
+        this.subfolders[i].destroy();
+    }
+
+    this.subfolders = [];
+}
+
+Folder.prototype.deleteFolder = function (folder) {
+    var fullname = folder.split("/");
+    // Es gibt keine tieferen Verzweigungen
+    if (fullname.length == 1) {
+
+        // Ordner finden
+        for (var i = 0; i < this.subfolders.length; i++) {
+            if (this.subfolders[i].name === folder) {
+                // und entsorgen
+                this.subfolders[i].destroy();
+                this.subfolders.splice(i, 1);
+
+                return;
+            }
+        }
+    }
+    else {
+        // Es gibt tiefere Strukturen
+        var lastPart = fullname.splice(1).join("/");
+        var firstPart = fullname[0];
+
+        // Richtigen Ordner raussuchen und der Unterstruktur die Bearbeitung übergeben
+        for (var i = 0; i < this.subfolders.length; i++) {
+            if (this.subfolders[i].name === firstPart) {
+                this.subfolders[i].deleteFolder(lastPart);
+                return;
+            }
+        }
+    }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,115 @@
-// Init Socket.IO
-var socket = io();
+'use strict';
 
-socket.on('file:new', function (fileinfo) {
-    console.log('New File: ' + fileinfo.relpath);
-});
+// Filedrop in den jQuery-Modus umschalten
+fd.logging = false;
+fd.jQuery();
 
-socket.on('folder:new', function (folderinfo) {
-    console.log('New Folder: ' + folderinfo.relpath);
-});
+angular.module('filemanager', ['angular-humanize'])
+.config(function () {
+})
+.factory('socket', function ($rootScope) {
+    var socket = io.connect();
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+})
+.directive('folderTree', function ($compile) {
+    return {
+        restrict: 'EA',
+        scope: {
+            folder: "="
+        },
+        link: function (scope, element, attrs) {
+            var dropzone, droptimer = false;
 
-socket.on('file:deleted', function (fileinfo) {
-    console.log('File Deleted: ' + fileinfo.relpath);
-});
+            scope.expanded = false;
 
-socket.on('folder:deleted', function (folderinfo) {
-    console.log('Folder Deleted: ' + folderinfo.relpath);
-});
+            scope.toggle = function () {
+                scope.expanded = !scope.expanded;
+            };
 
-socket.on('file:changed', function (folderinfo) {
-    console.log("File changed: " + folderinfo.relpath);
-});
+            var template = '<td colspan="3"><span ng-hide="expanded" class="glyphicon glyphicon-chevron-right"></span><span ng-show="expanded" class="glyphicon glyphicon-chevron-down"></span><span ng-click="toggle()">{{folder.name}}</span>' +
+                '<table ng-show="expanded" class="table table-striped table-hover">' +
+                '<tr class="upload-zone" style="display: none;"><td colspan="3"><div>Upload here...</div></td></tr>' +
+                '<tr folder-tree ng-repeat="item in folder.subfolders | orderBy:\'name\'" folder="item"></ng-sparkline>' +
+                '<tr class="file" ng-repeat="item in folder.files | orderBy:\'filename\'"><td>{{item.filename}}</td><td>{{item.size | humanizeFilesize}}</td><td>{{item.lastModified | date:\'dd.MM.yyyy HH:mm\'}}</td></tr>' +
+                '</table></td>';
+
+            //Rendering template.
+            element.html('').append($compile(template)(scope));
+
+            // Get the dropzone
+            dropzone = element.find('td > table .upload-zone');
+
+            // Show dropzone on drag and hide afterwards
+            element.on('dragover', function (e) {
+                if (e.originalEvent.dataTransfer.types !== null) {
+                    dropzone.show();
+                    clearTimeout(droptimer);
+                }
+            })
+            .on('dragleave', function (e) {
+                droptimer = setTimeout(function () {
+                    dropzone.hide();
+                }, 500);
+            });
+
+            element.find("td > table .upload-zone > td").filedrop()
+            // jQuery always passes event object as the first argument.
+            .on('fdsend', function (e, files) {
+                files.invoke('sendTo', 'upload?path=' + scope.folder.path)
+            })
+            .on('filedone', function (e, file) {
+                alert('Done uploading ' + file.name + ' on ' + scope.folder.path)
+            });
+        }
+    };
+})
+.controller('MainCtrl', function ($scope, $http, socket) {
+    window.scope = $scope;
+    $scope.rootFolder = new Folder("", "");
+
+    socket.on('connect', function () {
+        socket.emit("init");
+    })
+
+    socket.on('file:new', function (fileinfo) {
+        $scope.rootFolder.addFileFullName(fileinfo);
+    });
+
+    socket.on('folder:new', function (folderinfo) {
+        if (folderinfo.relpath !== '') {
+            $scope.rootFolder.addFolderFullName(folderinfo.relpath);
+        }
+    });
+
+    socket.on('file:deleted', function (fileinfo) {
+        $scope.rootFolder.deleteFileFull(fileinfo.relpath);
+    });
+
+    socket.on('folder:deleted', function (folderinfo) {
+        $scope.rootFolder.deleteFolder(folderinfo.relpath);
+    });
+
+    socket.on('file:changed', function (fileinfo) {
+        $scope.rootFolder.addFileFullName(fileinfo);
+    });
+})
+
